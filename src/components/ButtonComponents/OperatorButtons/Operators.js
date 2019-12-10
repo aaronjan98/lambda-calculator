ec3c6c54df360dde7a4e62649448b26843b13886
aaronjan <aaronjan98@gmail.com>
2019-12-10 14:34:56 -0800

    did the same for operators and specials

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -9,12 +9,16 @@ import { operators } from "../../../data";
 const Operators = () => {
   // STEP 2 - add the imported data to state
   const [operatorsState, setOperatorsState] = useState(operators);
+  //console.log("operator", operatorsState);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorsState.map((button, index) => (
+        <OperatorButton key={index} button={button} />
+      ))} 
     </div>
   );
 };
