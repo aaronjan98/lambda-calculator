a8731b3ef5e05f64faccdf2ed0edfb68adaed9a0
aaronjan <aaronjan98@gmail.com>
2019-12-10 15:05:24 -0800

    imported display.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <br></br>
         <Numbers />
         <Operators />
         <Specials />
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -5,7 +5,7 @@ const NumberButton = (props) => {
   return (
     <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-        {props.button}
+        <span>{props.button}</span>
     </button>
   );
 };
