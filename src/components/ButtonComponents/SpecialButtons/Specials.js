faec6bab801951a7c8e7693560468b2cc12c81b5
aaronjan <aaronjan98@gmail.com>
2019-12-10 12:24:42 -0800

    added useState to component files

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { specials } from "../../../data";
 
 //import any components needed
@@ -7,6 +7,7 @@ import { specials } from "../../../data";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialsState, setSpecialsState] = useState(specials);
 
   return (
     <div>
